958779546944a9e8429e050c8d5cee9d7d66a224
Pyae Phyo Aung <pyaephyoaungtcl.98@gmail.com>
2020-08-24 10:58:36 +0630

    first commit

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          :to="item.router"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <!-- <router-link :to="{path:item.router}">{{ item.text }}</router-link> -->
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left color="red" dense>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <!-- <v-icon class="mx-4" large>
+        mdi-account-circle-outline
+      </v-icon> -->
+      <v-toolbar-title class="mr-12 align-center">
+        <span class="title">Dashboard</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-filter</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row justify="center" align="center">
+          <v-col class="shrink">
+            <router-view></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { icon: "mdi-home", text: "Home", router: "/" },
+      { icon: "mdi-account-group", text: "ABout", router: "/about" },
+      { icon: "mdi-trending-up", text: "Trending" },
+      { icon: "mdi-history", text: "History" },
+    ],
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
